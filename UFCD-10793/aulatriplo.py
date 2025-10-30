10d0b367a22f8eb4bbd2b33e2d0a5f47ed51873b
Diogo <diogojulianapty@gmail.com>
2025-10-30 09:59:05 +0000

    upd

--- UFCD-10793/aulatriplo.py
+++ UFCD-10793/aulatriplo.py
@@ -5,3 +5,13 @@ triplo = [3 * x for x in numeros]
 
 print("Original:", numeros)
 print("Triplo:  ", triplo)
+
+#----------------------------------------
+
+numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+inteiros=[]
+
+inteiros = [(int(i)) for i in numeros]
+
+print(inteiros)
