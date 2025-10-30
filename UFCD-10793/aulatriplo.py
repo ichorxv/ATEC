52a4cce07fe92d33afbb7fe3279447b34c189280
Diogo <diogojulianapty@gmail.com>
2025-10-30 09:44:10 +0000

    upd

--- UFCD-10793/aulatriplo.py
+++ UFCD-10793/aulatriplo.py
@@ -1,7 +1,6 @@
-# lista original com 10 nÃºmeros inteiros
+
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# nova lista com o triplo de cada elemento (usando list comprehension)
 triplo = [3 * x for x in numeros]
 
 print("Original:", numeros)
